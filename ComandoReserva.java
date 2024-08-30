33e5d645ff178b1808f331a08ebadd43f9c59568
Netofu <98113246+Netofu@users.noreply.github.com>
2024-08-29 22:16:12 -0300

    Update ComandoReserva.java
    Colocando as Coisas em Ordem

--- ComandoReserva.java
+++ ComandoReserva.java
@@ -1,5 +1,34 @@
 class ComandoReserva implements Comando {
     public void executar(String[] args) {
+        SistemaBiblioteca biblioteca = SistemaBiblioteca.getInstance();
+        Usuario usuario = biblioteca.getUsuarioByCodigo(Integer.parseInt(args[1]));
+        Livro livro = biblioteca.getLivroByCodigo(Integer.parseInt(args[2]));
+        Exemplar exemplar = biblioteca.getExemplarByCodigoLivro(Integer.parseInt(args[2]));
+        if (usuario == null) {
+            GerenciadorIO.getInstance().PrintReserva("UsuarioNull");
+            return;
+        }
+        if (livro == null) {
+            GerenciadorIO.getInstance().PrintReserva("LivroNull");
+            return;
+        }
+        if (exemplar == null) {
+            GerenciadorIO.getInstance().PrintReserva("ExemplarNull");
+            return;
+        }
+        if (!usuario.isProfessor() && exemplar.isReserved() && !exemplar.isReservedTo(usuario.codigo)) {
+            GerenciadorIO.getInstance().PrintReserva("Reservado");
+            return;
+        }
+        
+        usuario.pegarEmprestado(exemplar);
+        exemplar.setIsReservado(true);
+        exemplar.setIsReservadoPara(usuario.getCodigo());
+        GerenciadorIO.getInstance().PrintReserva("Sucesso!");
+
+        
+    }
+}
         // Implementar lógica de reserva
         GerenciadorIO.getInstance().PrintReserva();
     }
