553961e80c701408478a61d6748cd49c2c35cce9
Netofu <98113246+Netofu@users.noreply.github.com>
2024-08-29 21:51:58 -0300

    Update ComandoConsultaUsuario.java

--- ComandoConsultaUsuario.java
+++ ComandoConsultaUsuario.java
@@ -1,6 +1,14 @@
 class ComandoConsultaUsuario implements Comando {
     public void executar(String[] args) {
+        SistemaBiblioteca biblioteca = SistemaBiblioteca.getInstance();
+        Usuario usuario = biblioteca.getUsuarioByCodigo(Integer.parseInt(args[1]));
+        Livro livro = biblioteca.getLivroByCodigo(Integer.parseInt(args[2]));
+        Exemplar exemplar = biblioteca.getExemplarByCodigoLivro(Integer.parseInt(args[2]));
+        if (usuario == null) {
+            GerenciadorIO.getInstance().PrintConsultaUsuario("UsuarioNull");
+            return;
+        }
+        GerenciadorIO.getInstance().PrintEmprestimo("Sucesso!");
         // Implementar lógica de consulta de usuário
-        GerenciadorIO.getInstance().PrintConsultaUsuario();
     }
 }
