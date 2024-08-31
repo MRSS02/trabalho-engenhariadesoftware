835733e529d3469b059dcbfbe25443d9247a7611
Netofu <98113246+Netofu@users.noreply.github.com>
2024-08-31 14:12:05 -0300

    Update ComandoConsultaUsuario.java

--- ComandoConsultaUsuario.java
+++ ComandoConsultaUsuario.java
@@ -8,8 +8,33 @@ class ComandoConsultaUsuario implements Comando {
             GerenciadorIO.getInstance().PrintConsultaUsuario("UsuarioNull");
             return;
         }
-        System.out.println(usuario.getLivrosReservados());
-        System.out.println(usuario.getLivrosEmprestados());
+        List<Exemplar> livrosReservados = usuario.getLivrosReservados();
+            if (livrosReservados.isEmpty()) {
+                System.out.println("Nenhum livro reservado.");
+            } else {
+                System.out.println("Livros reservados:");
+                for (Exemplar reservado : livrosReservados) {
+                    Livro reservado2 = reservado.getLivro();
+                    System.out.println(reservado2.getTitulo());
+                    System.out.println(reservado2.getQuantidadeReservas());
+                    System.out.println(reservado2.getDataReservada());
+                }
+            }
+
+            // Exibe os livros emprestados pelo usuário
+        List<Exemplar> livrosEmprestados = usuario.getLivrosEmprestados();
+            if (livrosEmprestados.isEmpty()) {
+                System.out.println("Nenhum livro emprestado.");
+            } else {
+                System.out.println("Livros emprestados:");
+                for (Exemplar emprestado : livrosEmprestados) {
+                    Livro emprestado2 = emprestado.getLivro();
+                    System.out.println(emprestado2.getTitulo());
+                    System.out.println(emprestado.getDataEmprestimo());
+                    System.out.println(emprestado.getDataDevolucaoPrevista())
+                }
+            }
+        
         GerenciadorIO.getInstance().PrintEmprestimo("Sucesso");
         // Implementar lógica de consulta de usuário
     }
