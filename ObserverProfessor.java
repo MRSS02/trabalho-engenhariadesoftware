2204f85f94cbf3284e7e1657c0f9af5002c12df9
MRSS02 <mateusyasd@hotmail.com>
2024-08-31 12:56:37 -0300

    readicionado contagem do número de vezes que o professor recebeu a notificação

--- ObserverProfessor.java
+++ ObserverProfessor.java
@@ -2,15 +2,18 @@ class ObserverProfessor implements Observer {
     private String nomeObservador;
     private String nomeLivro;
     private int codigoLivro;
+    private int vezesNotificado; 
 
     public ObserverProfessor(String nomeObservador, String nomeLivro, int codigoLivro) {
         this.nomeLivro = nomeLivro;
         this.nomeObservador = nomeObservador;
         this.codigoLivro = codigoLivro;
+        this.vezesNotificado = 0;
     }
 
     public void update(int codigoLivro) {
         if (this.codigoLivro == codigoLivro) {
+            this.vezesNotificado++;
             GerenciadorIO.getInstance().PrintNotificacaoProfessor(nomeObservador, nomeLivro);
         }
     }
